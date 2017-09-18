5264ab487123069547f9355d6f6b1769e5c277fd
denra <denra82@gmail.com>
2017-09-18 13:41:54 +0200

    Menu til consol
    En lille menu til programmet

--- BankingSystem/Program.cs
+++ BankingSystem/Program.cs
@@ -10,7 +10,20 @@ namespace BankingSystem
     {
         static void Main(string[] args)
         {
+            mainMenu();
+        }
 
+        public static void mainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Velkommen til banken");
+            Console.WriteLine("1. Tilføj ny kunde.");
+            Console.WriteLine("2. Find Kunde og konto.");
+            Console.WriteLine("3. Fjern kunde.");
+            Console.WriteLine("4. Redigere kunde.");
+            Console.WriteLine("5. Overfør penge");
+            Console.WriteLine("--------------------");
         }
     }
 }
